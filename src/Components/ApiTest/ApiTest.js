9d759fb765458ceedc1c6fb94a73f9286de4b264
ManiKumar Nakka <manikumarnakka324@gmail.com>
2024-07-25 11:59:23 +0530

    first commit

---
+++ src/Components/ApiTest/ApiTest.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import "./Apitest.css";
+import { ThreeDots } from 'react-loader-spinner';
+
+
+const ApiTest = () => {
+    const [data, setData] = useState([]);
+    const [filterdata, setFilterData] = useState([]);
+    const [load, setLoad] = useState(false);
+
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoad(true);
+            try {
+                const response = await fetch(url);
+                const result = await response.json();
+                setData(result);
+                setFilterData(result);
+            } catch (error) {
+                console.log(error);
+            }
+            setLoad(false);
+        }
+        fetchData();
+    }, []);
+
+    const handleChange = (e) => {
+        const searchedItem = e.target.value.toLowerCase();
+        const filtered = data.filter(item => item.title.toLowerCase().includes(searchedItem));
+        setLoad(true)
+        setTimeout(()=>{                       // here we have used setTimeout() method to load tghe list items for some time.
+            setFilterData(filtered)
+            setLoad(false)
+        },500)
+
+    }
+
+    return (
+        <div>
+            <div className='top-head'>API TESTING </div>
+            <div className='searchbar'>
+                <input type='search' onChange={handleChange} placeholder="You Can Search Here . . . . ." className='set-search form-control' />
+            </div>
+            <div className='list-items'>
+                {load ? (
+                    <div className='loader'><ThreeDots color="red" height={20} /></div>
+                ) : 
+                    (
+                    filterdata.map((item, index) => (
+                        <ol key={index} className='list-margin'>{index + 1}. {item.title}</ol>
+                        ))
+                    )
+                    }
+            </div>
+        </div>
+    )
+}
+
+export default ApiTest;
